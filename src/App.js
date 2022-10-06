89879bde78f0de73677eee7ec15cf6a9485288b3
Daniel Iovescu <iovescu.dani@gmail.com>
2022-10-06 20:25:59 +0300

    implemented react routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Home from './pages/Home';
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route component={PageNotFound}></Route>
+    </Switch>
   );
 }
 
