09c6eb17125314e0c531548ae4629e2c2b2e67af
Daniel Iovescu <iovescu.dani@gmail.com>
2022-11-03 17:46:27 +0200

    product details page

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import './ProductItem.css';
 
 function ProductItem({ product, category }) {
-    return <div className="col-4">
-        <img className="w-100" src={product.image} alt=""></img>
-        <h3 className="text-center">{product.name}</h3>
-        <h4 className="text-center">{product.price} {product.currency}</h4>
+    return <div className="product-item col-4 d-flex flex-column align-items-center mb-3">
+        <img src={product.image} alt="productPhoto" className="mb-2" />
+        <p className="mb-1 text-center">{product.name}</p>
+        <p className="text-center">{product.price + product.currency}</p>
         <div className="d-flex justify-content-center align-items-center">
             <Link to={`/products/${category}/${product.id}`}>
                 <button className="btn btn-link ">Cumpara!</button>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,7 +8,24 @@ function Product(props) {
     const items = products[category].items;
     const product = items.find(p => p.id === +productId);
     return <Layout>
-        {product.name}
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <img src={product.image} alt="" />
+                </div>
+                <div className="col-8">
+                    <h1>{product.name}</h1>
+                    <h4>{product.brand}</h4>
+                    <br></br>
+                    <p>{product.description}</p>
+                    <p>Marime: {product.size}</p>
+                    <p>Culoare: {product.colour}</p>
+                    <p>Material: {product.material}</p>
+                    <br></br>
+                    <button className="btn btn-secondary">Add to cart</button>
+                </div>
+            </div>
+        </div>
     </Layout>
 }
 
